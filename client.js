415f7c84b9cf1ede713dca030d831a4817020abf
Piotr <hynas321@gmail.com>
2023-01-26 16:36:56 +0100

    Simplify logic

--- client.js
+++ client.js
@@ -1,15 +1,57 @@
-const signInSubmitButton = document.getElementById("signInSubmit");
-const signUpSubmitButton = document.getElementById("signUpSubmit");
-const signInForm = document.getElementById("signInForm");
-const signUpForm = document.getElementById("signUpForm");
+const incorrectPasswordLengthMessage = document.getElementById("incorrect-password-length-message");
+const differentPasswordsMessage = document.getElementById("different-passwords-message");
+const minPasswordLength = 6
+const maxPasswordLength = 12
+const elementDisplayed = "block";
+const elementHidden = "none";
+const incorrectPasswordLengthMessageText = 
+    `Incorrect password length, allowed lengths: min = ${minPasswordLength} and max = ${maxPasswordLength}`;
 
-const formHandler = new FormHandler();
+function validateSignInForm(formData) {
+    const passwordString = formData.signInPassword.value;
 
-signInSubmitButton.addEventListener("click", function() {
-    formHandler.validateSignInForm(signInForm);
-});
+    if (!isPasswordLengthCorrect(passwordString)) {
+        displayIncorrectPasswordLengthMessage();
+    }
+    else if (incorrectPasswordLengthMessage.style.display === elementDisplayed) {
+        incorrectPasswordLengthMessage.style.display = elementHidden;
+    }
+    else {
+        //do something
+    }
+}
 
-signUpSubmitButton.addEventListener("click", function() {
-    formHandler.validateSignUpForm(signUpForm);
-});
+function validateSignUpForm(formData) {
+    const passwordString = formData.signUpPassword.value;
+    const repeatedPasswordString = formData.repeatedSignUpPassword.value;
+    
+    if (!(passwordString === repeatedPasswordString)) {
+        differentPasswordsMessage.style.display = elementDisplayed;
+    }
+    else if (!isPasswordLengthCorrect(passwordString) || !isPasswordLengthCorrect(repeatedPasswordString)) {
+        displayIncorrectPasswordLengthMessage();
+    }
+    else if (incorrectPasswordLengthMessage.style.display === elementDisplayed 
+        || differentPasswordsMessage.style.display === elementDisplayed) {
 
+        incorrectPasswordLengthMessage.style.display = elementHidden;
+        differentPasswordsMessage.style.display = elementHidden;
+    }
+    else {
+        //do something
+    }
+}
+
+function isPasswordLengthCorrect(passwordString) {
+    if (passwordString.length < minPasswordLength || passwordString.length > maxPasswordLength) {
+        return false;
+    }
+
+    return true;
+}
+
+function displayIncorrectPasswordLengthMessage() {
+    incorrectPasswordLengthMessage.style.display = elementDisplayed;
+    incorrectPasswordLengthMessage.textContent = incorrectPasswordLengthMessageText;
+       
+}
