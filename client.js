11ecf4ced46c8e727c984c96b7b76b5a2a62278a
Piotr <hynas321@gmail.com>
2023-02-01 10:56:39 +0100

    Major refactoring, add tabs styling, fix and change JS file

--- client.js
+++ client.js
@@ -1,6 +1,3 @@
-let userDataObject;
-let homeTabMessagesArray;
-
 const minPasswordLength = 6;
 const maxPasswordLength = 12;
 
@@ -9,13 +6,25 @@ const displayProperty = {
     none: "none"
 };
 
-const tokenKey = "token";
+const localStorageKey = {
+    token: "token",
+    lastOpenedTab: "lastOpenedTab"
+}
+
+const tabs = {
+    home: "home-tab",
+    account: "account-tab",
+    browse: "browse-tab"
+}
+
 const differentPasswordsText = "Passwords are not the same";
 const incorrectPasswordLengthText = 
     `Incorrect password length, allowed lengths: min = ${minPasswordLength} and max = ${maxPasswordLength}`;
 
-window.addEventListener("load", function() {
-    const tokenValue = this.localStorage.getItem(tokenKey);
+let dataRetrieved = false;
+
+window.onload = function() {
+    const tokenValue = localStorage.getItem(localStorageKey.token);
 
     if (typeof(tokenValue) != "string") {
         displayWelcomeView();
@@ -23,71 +32,77 @@ window.addEventListener("load", function() {
     else {
         displayProfileView(tokenValue);
     }
-});
+};
 
-function displayWelcomeView() {
+var displayWelcomeView = function() {
     const body = document.getElementById("body");
     const welcomeView = document.getElementById("welcomeview");
 
     body.innerHTML = welcomeView.innerHTML;
 }
 
-function displayProfileView() {
+var displayProfileView = function() {
+    const lastTabValue = localStorage.getItem(localStorageKey.lastOpenedTab);
     const body = document.getElementById("body");
     const profileView = document.getElementById("profileview");
 
     body.innerHTML = profileView.innerHTML;
 
-    displayHomeTab();
+    if (lastTabValue == tabs.home) {
+        displayHomeTab();
+    }
+    else if (lastTabValue == tabs.account) {
+        displayAccountTab();
+    }
+    else if (lastTabValue == tabs.browse) {
+        displayBrowseTab();
+    }
+    else {
+        displayHomeTab();
+    }
 }
 
-function displayHomeTab() {
+var displayHomeTab = function() {
     const homeTab = document.getElementById("home-tab");
 
     hidePreviousTabs();
+    setActiveButton(tabs.home);
+    setLocalStorageValue(localStorageKey.lastOpenedTab, tabs.home);
+
     homeTab.style.display = displayProperty.block;
 
-    const tokenValue = this.localStorage.getItem(tokenKey);
+    const tokenValue = localStorage.getItem(localStorageKey.token);
 
-    if (userDataObject == undefined) {
-        userDataObject = serverstub.getUserDataByToken(tokenValue);
-        console.log(userDataObject);
-    }
+    if (!dataRetrieved) {
+        const userEmail = serverstub.getUserDataByToken(tokenValue).data.email;
 
-    const email = document.getElementById("email-value");
-    const firstname = document.getElementById("firstname-value");
-    const familyName = document.getElementById("familyname-value");
-    const gender = document.getElementById("gender-value");
-    const city = document.getElementById("city-value");
-    const country = document.getElementById("country-value");
-
-    email.textContent = userDataObject.data.email;
-    firstname.textContent = userDataObject.data.firstname;
-    familyName.textContent = userDataObject.data.familyname;
-    gender.textContent = userDataObject.data.gender;
-    city.textContent = userDataObject.data.city;
-    country.textContent = userDataObject.data.country;
-
-    if (homeTabMessagesArray == undefined) {
-        displayAccountMessages(tokenValue, userDataObject.data.email);
+        setUserPersonalInfo();
+        displayPostWall(true, userEmail);
+        dataRetrieved = true;
     }
 }
 
-function displayAccountTab() {
+var displayAccountTab = function() {
     const accountTab = document.getElementById("account-tab");
 
     hidePreviousTabs();
+    setActiveButton(tabs.account);
+    setLocalStorageValue(localStorageKey.lastOpenedTab, tabs.account);
+
     accountTab.style.display = displayProperty.block;
 }
 
-function displayBrowseTab() {
+var displayBrowseTab = function() {
     const browseTab = document.getElementById("browse-tab");
 
     hidePreviousTabs();
+    setActiveButton(tabs.browse);
+    setLocalStorageValue(localStorageKey.lastOpenedTab, tabs.browse);
+
     browseTab.style.display = displayProperty.block;
 }
 
-function hidePreviousTabs() {
+var hidePreviousTabs = function() {
     const homeTab = document.getElementById("home-tab");
 
     if (homeTab.style.display == displayProperty.block) {
@@ -110,7 +125,7 @@ function hidePreviousTabs() {
     }
 }
 
-function validateSignInForm(signInFormData) {
+var signIn = function(signInFormData) {
     const emailString = signInFormData.signInEmail.value;
     const passwordString = signInFormData.signInPassword.value;
     const statusMessageElement = document.getElementById("status-message");
@@ -125,24 +140,24 @@ function validateSignInForm(signInFormData) {
         return;
     }
 
-    const signInServerOutputObj = serverstub.signIn(emailString, passwordString);
-    console.log(signInServerOutputObj);
+    const signInServerOutput = serverstub.signIn(emailString, passwordString);
+    console.log(signInServerOutput);
     
-    if (signInServerOutputObj.success == false) {
+    if (signInServerOutput.success == false) {
         displayStatusMessage(
             statusMessageElement,
-            signInServerOutputObj.message,
+            signInServerOutput.message,
             false
         );
 
         return;
     }
-    
-    localStorage.setItem(tokenKey, signInServerOutputObj.data);
+
+    localStorage.setItem(localStorageKey.token, signInServerOutput.data);
     displayProfileView();
 }
 
-function validateSignUpForm(signUpFormData) {
+var signUp = function(signUpFormData) {
     const passwordString = signUpFormData.signUpPassword.value;
     const repeatedPasswordString = signUpFormData.repeatedSignUpPassword.value;
     const statusMessageElement = document.getElementById("status-message");
@@ -166,23 +181,37 @@ function validateSignUpForm(signUpFormData) {
         return;
     }
 
-    const signUpServerOutputObj = signUp(signUpFormData);
-    console.log(signUpServerOutputObj);
+    const userDataObject = {
+        email: signUpFormData.signUpEmail.value,
+        password: signUpFormData.signUpPassword.value,
+        firstname: signUpFormData.firstName.value,
+        familyname: signUpFormData.familyName.value,
+        gender: signUpFormData.gender.value,
+        city: signUpFormData.city.value,
+        country: signUpFormData.country.value
+    };
 
-    if (signUpServerOutputObj.success == false) {
+    const signUpServerOutput = serverstub.signUp(userDataObject);
+    console.log(signUpServerOutput);
+
+    if (signUpServerOutput.success == false) {
         displayStatusMessage(
             statusMessageElement,
-            signUpServerOutputObj.message,
+            signUpServerOutput.message,
             false
         );
         
         return;
     }
 
-    displayProfileView();
+    displayStatusMessage(
+        statusMessageElement,
+        signUpServerOutput.message,
+        true
+    )
 }
 
-function isPasswordLengthCorrect(passwordString) {
+var isPasswordLengthCorrect = function(passwordString) {
     if (passwordString.length < minPasswordLength || passwordString.length > maxPasswordLength) {
         return false;
     }
@@ -190,7 +219,7 @@ function isPasswordLengthCorrect(passwordString) {
     return true;
 }
 
-function displayStatusMessage(statusMessageElement, statusMessage, success) {
+var displayStatusMessage = function(statusMessageElement, statusMessage, success) {
     if (success) {
         statusMessageElement.classList.replace("error-message", "success-message");
     }
@@ -202,36 +231,18 @@ function displayStatusMessage(statusMessageElement, statusMessage, success) {
     statusMessageElement.textContent = statusMessage;
 }
 
-function signUp(signUpFormData) {
-    const userDataObject = {
-        email: signUpFormData.signUpEmail.value,
-        password: signUpFormData.signUpPassword.value,
-        firstname: signUpFormData.firstName.value,
-        familyname: signUpFormData.familyName.value,
-        gender: signUpFormData.gender.value,
-        city: signUpFormData.city.value,
-        country: signUpFormData.country.value
-    };
-
-    return serverstub.signUp(userDataObject);
-}
-
-function signIn(emailString, passwordString) {
-    return serverstub.signIn(emailString, passwordString);
-}
-
-function signOut() {
-    const tokenValue = this.localStorage.getItem(tokenKey);
+var signOut = function() {
+    const tokenValue = this.localStorage.getItem(localStorageKey.token);
 
     console.log(serverstub.signOut(tokenValue));
-    localStorage.removeItem(tokenKey);
-    userDataObject = undefined;
-
+    
+    dataRetrieved = false;
+    clearLocalStorage();
     displayWelcomeView();
 }
 
-function changePassword(changePasswordFormData) {
-    const tokenValue = this.localStorage.getItem(tokenKey);
+var changePassword = function(changePasswordFormData) {
+    const tokenValue = this.localStorage.getItem(localStorageKey.token);
     const oldPassword = changePasswordFormData.oldPassword.value;
     const newPassword = changePasswordFormData.newPassword.value; 
     const repeatedNewPassword = changePasswordFormData.repeatedNewPassword.value;
@@ -260,14 +271,14 @@ function changePassword(changePasswordFormData) {
         return;
     }
 
-    const changePasswordServerOutputObj = serverstub.changePassword(tokenValue, oldPassword, newPassword);
+    const changePasswordServerOutput = serverstub.changePassword(tokenValue, oldPassword, newPassword);
 
-    console.log(changePasswordServerOutputObj);
+    console.log(changePasswordServerOutput);
 
-    if (changePasswordServerOutputObj.success) {
+    if (changePasswordServerOutput.success) {
         displayStatusMessage(
             accountTabStatusMessageElement,
-            changePasswordServerOutputObj.message,
+            changePasswordServerOutput.message,
             true
         );
 
@@ -276,98 +287,171 @@ function changePassword(changePasswordFormData) {
     else {
         displayStatusMessage(
             accountTabStatusMessageElement,
-            changePasswordServerOutputObj.message,
+            changePasswordServerOutput.message,
             false
         );
     }
 }
 
-function postMessage() {
-    const tokenValue = localStorage.getItem(tokenKey);
-    const textArea = document.getElementById("text-area");
-    const postWall = document.getElementById("post-wall");
-    const textDiv = document.createElement("div");
-    const email = userDataObject.email;
+var postMessageToWall = function(ownPostWall) {
+    const tokenValue = localStorage.getItem(localStorageKey.token);
+    let email;
+    let recipientEmail;
+    let postWall;
+    let textArea;
+    
+    if (ownPostWall) {
+        postWall = document.getElementById("post-wall");
+        textArea = document.getElementById("text-area");
 
-    textDiv.innerHTML = textArea.value;
+        if (textArea.value == "") {
+            return;
+        }
 
-    if (userDataObject == undefined || userDataObject.length == 0) {
-        serverstub.postMessage(tokenValue, textArea.value, email);
-        postWall.appendChild(textDiv);
+        email = serverstub.getUserDataByToken(tokenValue).data.email;
+        recipientEmail = email;
     }
     else {
-        const firstElement = document.getElementById("message-0");
-        
-        serverstub.postMessage(tokenValue, textArea.value, email);
-        postWall.insertBefore(textDiv, firstElement);
+        postWall = document.getElementById("searched-post-wall");
+        textArea = document.getElementById("searched-text-area");
+
+        if (textArea.value == "") {
+            return;
+        }
+
+        email = serverstub.getUserDataByToken(tokenValue).data.email;
+        recipientEmail = document.getElementById("searchedEmail").value;
     }
 
-    textArea.value = "";
+    console.log(serverstub.postMessage(tokenValue, textArea.value, recipientEmail));
+
+    if (ownPostWall) {
+        displayPostWall(ownPostWall, email);
+    }
+    else {
+        displayPostWall(ownPostWall, recipientEmail);
+    }
+
 }
 
-function displayAccountMessages(tokenValue, userEmail) {
-    homeTabMessagesArray = serverstub.getUserMessagesByEmail(tokenValue, userEmail).data;
-    console.log(homeTabMessagesArray);
+var displayPostWall = function(ownPostWall, email) {
+    const tokenValue = localStorage.getItem(localStorageKey.token);
+    let userMessages;
+    let postWall;
 
-    const postWall = document.getElementById("post-wall");
+    if (ownPostWall) {
+        userMessages = serverstub.getUserMessagesByToken(tokenValue).data;
+        postWall = document.getElementById("post-wall");
+    }
+    else {
+        userMessages = serverstub.getUserMessagesByEmail(tokenValue, email).data;
+        postWall = document.getElementById("searched-post-wall");
+    };
 
-    for (let i = 0; i < homeTabMessagesArray.length; i++) {
-        const textDiv = document.createElement("div");
+    console.log("Post wall displayed");
 
-        textDiv.setAttribute("id", `message-${i}`)
-        textDiv.innerHTML = homeTabMessagesArray[i].content;
-        postWall.appendChild(textDiv);
+    postWall.innerHTML = "";
+
+    for (let i = 0; i < userMessages.length; i++) {
+        postWall.innerHTML += `<br><b><div style="color:red">${userMessages[i].writer}</b></div> ${userMessages[i].content}<br>`;
     }
 }
 
-function displaySearchedUserProfile(browseFormDataObject) {
-    const tokenValue = this.localStorage.getItem(tokenKey);
-    const inputEmail = browseFormDataObject.searchedEmail.value;
+var refreshPostWall = function(tabName) {
+    if (tabName == tabs.home) {
+        displayPostWall(true, null);
+    }
+    else if (tabName == tabs.browse) {
+        const recipientEmail = document.getElementById("searchedEmail").value;
+
+        displayPostWall(false, recipientEmail);
+    }
+}
+
+var setUserPersonalInfo = function() {
+    const tokenValue = localStorage.getItem(localStorageKey.token);
+    const userData = serverstub.getUserDataByToken(tokenValue);
+
+    console.log(userData);
+
+    document.getElementById("email-value").textContent = userData.data.email;
+    document.getElementById("firstname-value").textContent = userData.data.firstname;
+    document.getElementById("familyname-value").textContent = userData.data.familyname;
+    document.getElementById("gender-value").textContent = userData.data.gender;
+    document.getElementById("city-value").textContent = userData.data.city;
+    document.getElementById("country-value").textContent = userData.data.country;
+}
+
+var setSearchedUserPersonalInfo = function(userDataByEmailServerOutput) {
+    document.getElementById("searched-email-value").textContent = userDataByEmailServerOutput.data.email;
+    document.getElementById("searched-firstname-value").textContent = userDataByEmailServerOutput.data.firstname;
+    document.getElementById("searched-familyname-value").textContent = userDataByEmailServerOutput.data.familyname;
+    document.getElementById("searched-gender-value").textContent = userDataByEmailServerOutput.data.gender;
+    document.getElementById("searched-city-value").textContent = userDataByEmailServerOutput.data.city;
+    document.getElementById("searched-country-value").textContent = userDataByEmailServerOutput.data.country;
+}
+
+var displaySearchedUserProfile = function(browseFormDataObject) {
     const embeddedTab = document.getElementById("embedded-tab");
     const browseTabStatusMessageElement = document.getElementById("browse-tab-status-message");
+    const tokenValue = localStorage.getItem(localStorageKey.token);
+    const email = browseFormDataObject.searchedEmail.value;
 
-    const getUserDataByEmailOutputObj = serverstub.getUserDataByEmail(tokenValue, inputEmail);
-    console.log(getUserDataByEmailOutputObj);
+    const userDataByEmailServerOutput = serverstub.getUserDataByEmail(tokenValue, email);
 
-    if (getUserDataByEmailOutputObj.success == false) {
+    console.log(userDataByEmailServerOutput);
+
+    if (userDataByEmailServerOutput.success == false) {
         embeddedTab.style.display = displayProperty.none;
 
         displayStatusMessage(
             browseTabStatusMessageElement,
-            getUserDataByEmailOutputObj.message,
+            userDataByEmailServerOutput.message,
             false
         );
 
         return;
     }
 
-    const email = document.getElementById("searched-email-value");
-    const firstname = document.getElementById("searched-firstname-value");
-    const familyName = document.getElementById("searched-familyname-value");
-    const gender = document.getElementById("searched-gender-value");
-    const city = document.getElementById("searched-city-value");
-    const country = document.getElementById("searched-country-value");
+    if (browseTabStatusMessageElement.style.display == displayProperty.block) {
+        browseTabStatusMessageElement.style.display = displayProperty.none;
+    } 
     
-    email.textContent = getUserDataByEmailOutputObj.data.email;
-    firstname.textContent = getUserDataByEmailOutputObj.data.firstname;
-    familyName.textContent = getUserDataByEmailOutputObj.data.familyname;
-    gender.textContent = getUserDataByEmailOutputObj.data.gender;
-    city.textContent = getUserDataByEmailOutputObj.data.city;
-    country.textContent = getUserDataByEmailOutputObj.data.country;
+    setSearchedUserPersonalInfo(userDataByEmailServerOutput);
 
     embeddedTab.style.display = displayProperty.block;
 
-    const postWall = document.getElementById("browse-post-wall");
-
-    for (let i = 0; i < homeTabMessagesArray.length; i++) {
-        const textDiv = document.createElement("div");
-
-        textDiv.setAttribute("id", `message-${i}`)
-        textDiv.innerHTML = homeTabMessagesArray[i].content;
-        postWall.appendChild(textDiv);
-    }
+    displayPostWall(false, email);
 }
 
-function removeMessageNodes() {
-    
+var setLocalStorageValue = function(key, stringValue) {
+    localStorage.removeItem(key);
+    localStorage.setItem(key, stringValue);
+}
+
+var clearLocalStorage = function() {
+    localStorage.removeItem(localStorageKey.token);
+    localStorage.removeItem(localStorageKey.lastOpenedTab);
+}
+
+var setActiveButton = function(tabName) {
+    const homeButton = document.getElementById("btn-home-tab");
+    const accountButton = document.getElementById("btn-account-tab");
+    const browseButton = document.getElementById("btn-browse-tab");
+
+    if (tabName == tabs.home) {
+        homeButton.style.backgroundColor = "#53e058";
+        accountButton.style.backgroundColor = "#22c0f4";
+        browseButton.style.backgroundColor = "#22c0f4";
+    }
+    else if (tabName == tabs.account) {
+        homeButton.style.backgroundColor = "#22c0f4";
+        accountButton.style.backgroundColor = "#53e058";
+        browseButton.style.backgroundColor = "#22c0f4";
+    }
+    else if (tabName == tabs.browse) {
+        homeButton.style.backgroundColor = "#22c0f4";
+        accountButton.style.backgroundColor = "#22c0f4";
+        browseButton.style.backgroundColor = "#53e058";
+    }
 }
